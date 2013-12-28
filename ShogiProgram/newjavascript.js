91727d63e78a920accd6f94ec7ff4f0b8e740157
matanabe <drosophila.7ishidate3.cos-mos@docomo.ne.jp>
2013-12-28 18:18:06 +0900

    Signed-off-by: matanabe <drosophila.7ishidate3.cos-mos@docomo.ne.jp>

---
+++ ShogiProgram/newjavascript.js
@@ -0,0 +1,12 @@
+aa/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('');
+}).listen('janken.js');
+console.log('Server running at http:janken.js');
